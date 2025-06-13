fbb46fc0a8733844e08a3e2f622da506b830c834
Toundé Adédjouma <tounde.adedjouma@outlook.com>
2025-06-13 17:17:40 +0200

    chore(project): Initial commit

---
+++ src/API/PokedexApi.js
@@ -0,0 +1,2 @@
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
+const pkmnImg = import.meta.env.VITE_API_PKMN_IMG;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './components/pokedex.module.css'
+import './global.css'
+
+export function App() {
+
+  return (
+    <div style={{ height: '100%' }}>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
